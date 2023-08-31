42fd849f4638d2046df75a5525e745bad379ee42
Nenoeglah <eglah.chepngeno@student.moringaschool.com>
2023-09-01 01:02:23 +0300

    completed assignment

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,43 @@
-#!/usr/bin/env python3
 
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+ 
+    engine = create_engine('sqlite:///students.db')
+
+  
+    Base.metadata.create_all(engine)
+
+  
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    
+    students_to_insert = ['Milgo Chepngeno', 'Eglah Kitty', 'Georgia', 'Ginny']
+    for student_name in students_to_insert:
+        student = session.query(Student).filter_by(name=student_name).first()
+        if not student:
+            new_student = Student(name=student_name)
+            session.add(new_student)
+            session.commit()
+            print(f"{student_name} .")
+
+
+    students = session.query(Student).all()
+    print("List of Students:")
+    for student in students:
+        print(f"Student ID: {student.id}, Name: {student.name}")
+
+   
+    session.close()
+
